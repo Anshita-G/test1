b42e17d9c2b2147082e86748d82c36dee8525255
Abhay Gangwal <abhay.gangwal@hanu.com>
2023-06-01 12:31:15 +0530

    first push

---
+++ storage-test/controls/storage.rb
@@ -0,0 +1,11 @@
+control "storage account" do                                
+    impact 1.0                                                                   
+    title "Check storage account exist and its logging version"  
+
+    describe azure_storage_account(resource_group: 'a-test', name: 'abhay98098')  do
+        it { should exist }
+    end
+    describe azure_storage_account(resource_group: 'a-test', name: 'abhay98098') do
+        its('blob_properties.logging.version') { should cmp '1.0' }
+    end
+end
