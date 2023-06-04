47d652c7a671c8b1502e3c44cf5b11807c127fa4
Anshita-G <69114104+Anshita-G@users.noreply.github.com>
2023-06-04 18:57:07 +0530

    Update storage.rb

--- controls/storage.rb
+++ controls/storage.rb
@@ -2,12 +2,6 @@ describe azure_resource_group(name: 'atest') do
     it { should exist }
 end
 
-control "storage account" do                                
-    impact 1.0                                                                   
-    title "Check storage account exist and its logging version"  
-
-    describe azure_storage_account(resource_group: 'atest', name: 'plmkobh988742')  do
+describe azure_storage_account(resource_group: 'atest', name: 'plmkobh988742')  do
         it { should exist }
     end
-
-end
